ae9199fcc075a7be3f0ba68474e19745cfca3cbc
diana <dianetodirica@gmail.com>
2023-09-13 09:54:57 +0300

    Add transaction

--- helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/PmHelper.java
+++ helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/PmHelper.java
@@ -3,13 +3,12 @@ package org.jboss.as.quickstarts.helloworld;
 import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManager;
 import javax.jdo.PersistenceManagerFactory;
-import javax.jdo.Transaction;
+import javax.naming.NamingException;
 
 public interface PmHelper {
 
     static <T> T usePM(FunctionWithException<PersistenceManager, T> fn) {
         PersistenceManagerFactory factory = JDOHelper.getPersistenceManagerFactory("hello-world");
-        //@PersistenceContext(type = PersistenceContextType.EXTENDED, unitName="hello-world")
         PersistenceManager pm = factory.getPersistenceManager();
         try {
             return fn.apply(pm);
@@ -21,18 +20,16 @@ public interface PmHelper {
 
     }
 
-    static void populateDb(PersistenceManager em) {
-        Transaction tx = em.currentTransaction();
-        try {
-            tx.begin();
-            HelloWorld hw1 = new HelloWorld(1, "Hello");
-            HelloWorld hw2 = new HelloWorld(2, "Bonjour");
-            em.makePersistent(hw1);
-            em.makePersistent(hw2);
-            tx.commit();
-        } finally {
-            if (tx.isActive()) tx.rollback();
-        }
+    static void populateDb(PersistenceManager em) throws NamingException {
+//    UserTransaction ut = (UserTransaction) new InitialContext().lookup("java:comp/UserTransaction");
+//        Transaction tx = em.currentTransaction();
+//        tx.begin();
+        HelloWorld hw1 = new HelloWorld(1, "Hello");
+        HelloWorld hw2 = new HelloWorld(2, "Bonjour");
+        em.makePersistent(hw1);
+        em.makePersistent(hw2);
+        em.flush();
+//        tx.commit();
     }
 
     static <T> T usePopulatedDb(FunctionWithException<PersistenceManager, T> fn) {
